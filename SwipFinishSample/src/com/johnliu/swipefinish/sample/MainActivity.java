f9cdfe6ced9189ea424afab778bc513c2e73f7e1
Eric <johnliu.lgq@gmail.com>
2014-06-24 21:03:35 +0800

    fixed Test

--- SwipFinishSample/src/com/johnliu/swipefinish/sample/MainActivity.java
+++ SwipFinishSample/src/com/johnliu/swipefinish/sample/MainActivity.java
@@ -7,48 +7,51 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+/**
+ * 
+ * MainActivity
+ * 
+ * Created by John on 2014Äê6ÔÂ24ÈÕ
+ */
 public class MainActivity extends Activity implements OnClickListener {
 
-	private Button btnTest;
+    private Button btnTest;
 
-	private Button btnListView;
+    private Button btnListView;
 
-	private Button btnViewPager;
+    private Button btnViewPager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initViews();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btn_common:
-			startActivity(new Intent(getApplicationContext(),
-					CommonActivity.class));
-			break;
-		case R.id.btn_listview:
-			startActivity(new Intent(getApplicationContext(),
-					ListViewActivity.class));
-			break;
-		case R.id.btn_viewpager:
-			startActivity(new Intent(getApplicationContext(),
-					ViewPagerActivity.class));
-			break;
-		default:
-			break;
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_common:
+                startActivity(new Intent(getApplicationContext(), CommonActivity.class));
+                break;
+            case R.id.btn_listview:
+                startActivity(new Intent(getApplicationContext(), ListViewActivity.class));
+                break;
+            case R.id.btn_viewpager:
+                startActivity(new Intent(getApplicationContext(), ViewPagerActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
 
-	private void initViews() {
-		btnTest = (Button) this.findViewById(R.id.btn_common);
-		btnListView = (Button) this.findViewById(R.id.btn_listview);
-		btnViewPager = (Button) this.findViewById(R.id.btn_viewpager);
-		btnTest.setOnClickListener(this);
-		btnListView.setOnClickListener(this);
-		btnViewPager.setOnClickListener(this);
-	}
+    private void initViews() {
+        btnTest = (Button) this.findViewById(R.id.btn_common);
+        btnListView = (Button) this.findViewById(R.id.btn_listview);
+        btnViewPager = (Button) this.findViewById(R.id.btn_viewpager);
+        btnTest.setOnClickListener(this);
+        btnListView.setOnClickListener(this);
+        btnViewPager.setOnClickListener(this);
+    }
 
 }
