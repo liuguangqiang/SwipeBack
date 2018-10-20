ef7d005ec9ba2013142861f8aa351412cb9a5100
Julio Olivares <julio@n3twork.com>
2018-10-20 10:51:47 -0300

    adding finish listener to demo activity

--- app/src/main/java/com/liuguangqiang/swiplebacksample/DemoActivity.java
+++ app/src/main/java/com/liuguangqiang/swiplebacksample/DemoActivity.java
@@ -9,10 +9,9 @@ import com.liuguangqiang.swipeback.SwipeBackLayout;
 /**
  * Created by Eric on 15/2/27.
  */
-public class DemoActivity extends Activity {
+public class DemoActivity extends Activity implements SwipeBackLayout.OnFinishListener {
 
     private CircleProgressBar progressBar;
-    private SwipeBackLayout swipeBackLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +22,9 @@ public class DemoActivity extends Activity {
 
     private void initViews() {
         progressBar = (CircleProgressBar) findViewById(R.id.progressbar1);
-        swipeBackLayout = (SwipeBackLayout) findViewById(R.id.swipe_layout);
+        SwipeBackLayout swipeBackLayout = (SwipeBackLayout) findViewById(R.id.swipe_layout);
+        swipeBackLayout.setOnFinishListener(this);
         swipeBackLayout.setEnableFlingBack(false);
-
         swipeBackLayout.setOnPullToBackListener(new SwipeBackLayout.SwipeBackListener() {
             @Override
             public void onViewPositionChanged(float fractionAnchor, float fractionScreen) {
@@ -34,4 +33,9 @@ public class DemoActivity extends Activity {
         });
     }
 
+    @Override
+    public void onFinishState() {
+        //Do what you want to do before finish the activity
+        finish();
+    }
 }
